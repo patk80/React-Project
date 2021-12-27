5e3fd74bddeb9254517b271f809fbfd552d516f7
Pat Kelly <PatK80@gmail.com>
2021-12-26 22:10:43 -0500

    Updating all images to have higher quality

--- react-project/src/Discography.js
+++ react-project/src/Discography.js
@@ -5,23 +5,23 @@ import './Discography.css';
 class Discography extends Component{
     static defaultProps = {
         record : [
-            {albumName: "Iron Maiden", albumYear: 1980, albumCover: "https://upload.wikimedia.org/wikipedia/en/7/7c/Iron_Maiden_%28album%29_cover.jpg"},
-            {albumName: "Killers", albumYear: 1981, albumCover: "https://upload.wikimedia.org/wikipedia/en/b/b4/Iron_Maiden_Killers.jpg"},
-            {albumName: "Number of the Beast", albumYear: 1982, albumCover: "https://img.discogs.com/YW7nVVIdLqA7-iXsJfWe4hYiMjA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-4225106-1576052157-3898.jpeg.jpg"},
-            {albumName: "Piece of Mind", albumYear: 1983, albumCover: "https://upload.wikimedia.org/wikipedia/en/8/85/Iron_Maiden_-_Piece_Of_Mind.jpg"},
-            {albumName: "Powerslave", albumYear: 1984, albumCover: "https://upload.wikimedia.org/wikipedia/en/1/1c/Iron_Maiden_-_Powerslave.jpg"},
-            {albumName: "Caught Somewhere in Time", albumYear: 1986, albumCover: "https://upload.wikimedia.org/wikipedia/en/9/9b/Iron_Maiden_-_Somewhere_in_Time.jpg"},
-            {albumName: "Seventh Son of a Seventh Son", albumYear: 1988, albumCover: "https://upload.wikimedia.org/wikipedia/en/c/c0/Iron_Maiden_-_Seventh_Son_Of_A_Seventh_Son.jpg"},
-            {albumName: "No Prayer For the Dying", albumYear: 1990, albumCover: "https://upload.wikimedia.org/wikipedia/en/5/5c/IronMaidenNoPrayerForTheDying.jpg"},
-            {albumName: "Fear of the Dark", albumYear: 1992, albumCover: "https://upload.wikimedia.org/wikipedia/en/e/eb/Iron_Maiden_-_Fear_Of_The_Dark.jpg"},
-            {albumName: "The X Factor", albumYear: 1995, albumCover: "https://upload.wikimedia.org/wikipedia/en/2/21/Iron_Maiden_-_The_X_Factor.jpg"},
-            {albumName: "Virtual XI", albumYear: 1998, albumCover: "https://upload.wikimedia.org/wikipedia/en/3/3a/Iron_Maiden_-_Virtual_XI.jpg"},
-            {albumName: "Brave New World", albumYear: 2000, albumCover: "https://img.discogs.com/Mi9OTZcGpuX3FGoUpCFdwlkqTQM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12056902-1626649316-8698.jpeg.jpg"},
-            {albumName: "Dance of Death", albumYear: 2003, albumCover: "https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Iron_Maiden-Dance_of_Death.jpg/220px-Iron_Maiden-Dance_of_Death.jpg"},
-            {albumName: "A Matter of Life and Death", albumYear: 2006, albumCover: "https://upload.wikimedia.org/wikipedia/en/7/77/Iron_Maiden_-_A_Matter_Of_Life_And_Death.jpg"},
-            {albumName: "The Final Frontier", albumYear: 2010, albumCover: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Final_Frontier_cover.jpg"},
-            {albumName: "The Book of Souls", albumYear: 2015, albumCover: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Iron_Maiden_-_The_Book_of_Souls.png/220px-Iron_Maiden_-_The_Book_of_Souls.png"},
-            {albumName: "Senjutsu", albumYear: 2021, albumCover: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Iron_Maiden_-_Senjutsu.jpg/220px-Iron_Maiden_-_Senjutsu.jpg"}
+            {albumName: "Iron Maiden", albumYear: 1980, albumCover: "https://m.media-amazon.com/images/I/81J4jpxoScL._SL1500_.jpg"},
+            {albumName: "Killers", albumYear: 1981, albumCover: "https://m.media-amazon.com/images/I/81foTapQNbL._AC_SL1500_.jpg"},
+            {albumName: "The Number of the Beast", albumYear: 1982, albumCover: "https://townsquare.media/site/846/files/2017/03/Number-of-the-beast-album-cover.jpg"},
+            {albumName: "Piece of Mind", albumYear: 1983, albumCover: "https://m.media-amazon.com/images/I/819CkMmorLL._SL1500_.jpg"},
+            {albumName: "Powerslave", albumYear: 1984, albumCover: "https://m.media-amazon.com/images/I/81qKQEXFRSL._AC_SL1500_.jpg"},
+            {albumName: "Caught Somewhere in Time", albumYear: 1986, albumCover: "https://lastfm.freetls.fastly.net/i/u/ar0/44711959e4d890ff1d99045d5ff3dff1.jpg"},
+            {albumName: "Seventh Son of a Seventh Son", albumYear: 1988, albumCover: "https://m.media-amazon.com/images/I/81MnhzRHYwL._AC_SL1500_.jpg"},
+            {albumName: "No Prayer For the Dying", albumYear: 1990, albumCover: "https://m.media-amazon.com/images/I/81yi+KqZ9SL._AC_SL1500_.jpg"},
+            {albumName: "Fear of the Dark", albumYear: 1992, albumCover: "https://m.media-amazon.com/images/I/81xEMRQkfpL._SL1500_.jpg"},
+            {albumName: "The X Factor", albumYear: 1995, albumCover: "https://lh3.googleusercontent.com/proxy/864qPt8H9ZK_ccYQoKohTiROjWPOxuyxHXM2x4CP0f1lbGlJ-QfUaFVZd6PMsbNKTOAlqs1qZpNTh_njELo5AekQNRiS5uVYoC3kFop-rBYuwcy-6Ac9WBj7g3Tg2Q_IeIAUgcqTO6bq1_jJWxXbsWFI-rmGvsc"},
+            {albumName: "Virtual XI", albumYear: 1998, albumCover: "https://m.media-amazon.com/images/I/81VxO9sHLfL._SL1300_.jpg"},
+            {albumName: "Brave New World", albumYear: 2000, albumCover: "https://d24jnm9llkb1ub.cloudfront.net/icpn/4050538293180/4050538293180-cover-zoom.jpg"},
+            {albumName: "Dance of Death", albumYear: 2003, albumCover: "https://lh3.googleusercontent.com/proxy/0JNnQQ3Le-sctJcJSciqkJKO6eD8Fo-0F56DadOMdXDeimudTA3c4nhl6TYj76bLyAUjcojcdfxxoY0OoRrIVjP2VkLVTnkz4thVmk4dzrx_hD236A8-BSs8bmzAMEDPYeiw-0AkDdMeGkutfCbA17F6XRJPmA"},
+            {albumName: "A Matter of Life and Death", albumYear: 2006, albumCover: "https://lh3.googleusercontent.com/proxy/zOfAQ54Wxm0I6yR-ukpSVgnAqMqFmimkpVRpesmNI1lFSKXi4jPkE9R_7bYG5vk1_v8F9ZGcjvHY5-KGX9RV2eqrXeXPbt-Nu7WQyXcR_V201EeXuHjEN2MmQJapGGgSbD0X426L5zx4Q0bMgNMCRBwA-TpUF6w"},
+            {albumName: "The Final Frontier", albumYear: 2010, albumCover: "https://townsquare.media/site/295/files/2020/08/FinalFrontier.jpg?w=980&q=75"},
+            {albumName: "The Book of Souls", albumYear: 2015, albumCover: "https://lh3.googleusercontent.com/proxy/-3StnA6NAKH3wVtlAUktC2-Z0BQi6-2S_3iULhLXRROHFJ7rNvyqqSTp3xL93ZlSBNzRX68dxeAXgw_DyOI-lq211-rNmd2CEh6VwdFwfBsL5mtuOWepEeJ65xPyDbpWv1MgdMBlU43rrUpwtAGH_VAc8yMr1cM"},
+            {albumName: "Senjutsu", albumYear: 2021, albumCover: "https://www.ironmaiden.com/media/images/teddy.jpg"}
         ]
     };
     render(){
